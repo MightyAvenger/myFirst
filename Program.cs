f75ec429dea661a6866ad9440b23c9d43b464514
Kiril <chebukhk@gmail.com>
2024-09-17 12:44:03 +0300

    first version

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace project;
+ 
+class Program {
+
+
+    public static void Main()
+    { 
+       
+        System.Console.WriteLine("ee");
+        
+
+    } 
+   
+   
+}
+    
+    
+
+
+
+
+
+
+
